3c63c27e6e836f8f7e63ba003e6cece982f52455
dmleonov-cloudlinux <95280336+dmleonov-cloudlinux@users.noreply.github.com>
2023-04-26 11:18:49 +0200

    Added auth

--- handler.js
+++ handler.js
@@ -6,6 +6,10 @@ const getCritical = require('./src/getCritical');
 const minify = require('./src/minify');
 
 module.exports.processor = async (event) => {
+    if (process.env.X_API_KEY !== event.headers.x-api-key) {
+        throw new Error("Unauthorized access");
+    }
+    
     const eventBody = JSON.parse(event.body);
     const url = eventBody.url;
     const key = eventBody.key;
